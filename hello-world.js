b02a4e2fa618cf412d14cbefc06a7244e824079f
Brant Oliver <boliver@starbucks.com>
2016-10-30 11:51:17 -0700

    Updating port variable

--- hello-world.js
+++ hello-world.js
@@ -3,4 +3,4 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
-server.listen(8000);
+server.listen(process.env.PORT);
