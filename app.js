d26dac83247c3a76af8c69de7d18a43f3bedd8d3
Brant Oliver <boliver@starbucks.com>
2016-10-30 13:25:28 -0700

    Changing message.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World + From Azure!\n");
+  response.end("Hello World from Azure!\n");
 });
 server.listen(process.env.PORT);
